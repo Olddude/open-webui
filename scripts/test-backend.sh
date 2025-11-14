4c758ba39b987537974394673cf1d3083d6eb649
Anonymous Universus <anonymous.universus@outlook.com>
2025-11-14 11:05:17 +0100

    fix

---
+++ scripts/test-backend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+set -e
+
+script_file_path="${BASH_SOURCE[0]}"
+script_dir_path="$(cd "$(dirname "$script_file_path")" && pwd)"
+root_dir_path="$(cd "$script_dir_path/.." && pwd)"
+
+if [ "$(pwd)" != "$root_dir_path" ]; then
+    cd "$root_dir_path"
+fi
+
+if [ -f .venv/bin/activate ]; then
+    source .venv/bin/activate
+fi
+
+if [ -f .env ]; then
+    set -a
+    source .env
+    set +a
+fi
+
+cd backend
+
+python -m pytest test/
